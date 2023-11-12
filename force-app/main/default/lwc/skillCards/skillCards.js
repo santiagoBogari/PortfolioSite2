d6b3594d00754cf98669e8a07da80caec080bd70
santiagobogari <santiagobogari@gmail.com>
2023-11-12 15:46:43 -0300

    skills LWC

---
+++ force-app/main/default/lwc/skillCards/skillCards.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class SkillCards extends LightningElement {
+
+    img1_Url= IMAGES + '/card1.png';
+    img2_Url= IMAGES + '/card2.png';
+    img3_Url= IMAGES + '/card3.png';
+}
